a87423182e02d40e0ca1082bdf6c57bfa5efa95b
Vishnu Prabu P <vishnuprabup@gmail.com>
2023-09-09 00:38:37 +0530

    version 1.0

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/app/store";
